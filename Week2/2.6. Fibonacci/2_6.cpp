75de3be7815b694b43a060fb47a428f6345eac26
Hà Vĩnh Phước <122790758+Kas2k3@users.noreply.github.com>
2023-11-21 16:15:04 +0700

    code fibonacci voi de quy

--- Week2/2.6. Fibonacci/2_6.cpp
+++ Week2/2.6. Fibonacci/2_6.cpp
@@ -13,7 +13,7 @@ int fibonacci(int a[], int n) {
       
     }
 
-    //Ham de quy tinh toan fibonacci voi so >= 2
+    //Ham de quy tinh toan fibonacci voi so >= 2 
     if(n >= 2) {
         a[n] = fibonacci(a,n-1) + fibonacci(a,n-2);
         
