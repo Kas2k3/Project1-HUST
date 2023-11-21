dd662616b0e6fb1dfc90c24c60008916b578d609
Hà Vĩnh Phước <122790758+Kas2k3@users.noreply.github.com>
2023-11-21 15:50:50 +0700

    them comment cho bai sudoku

--- Week2/2.5. Count number of sudoko solutions/2_5.cpp
+++ Week2/2.5. Count number of sudoko solutions/2_5.cpp
@@ -5,16 +5,19 @@
 
 int count = 0;
 
+//Ham kiem tra gia tri thoa man
 int is_valid(int t[N][N], int row, int col, int num) {
-
+    //Kiem tra num da xuat hien o hang va cot nao chua
     for (int i = 0; i < N; i++) {
         if (t[row][i] == num || t[i][col] == num) {
             return 0;
         }
     }
 
+    //Xet cac o sub-grid 3x3
     int start_row = row - (row % 3);
     int start_col = col - (col % 3);
+    //Kiem tra co ton tai (row, col) trong sub-grid 3x3 khong
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
             if (t[i + start_row][j + start_col] == num) {
@@ -22,34 +25,35 @@ int is_valid(int t[N][N], int row, int col, int num) {
             }
         }
     }
-
+    //Neu thoa man thi danh dau da dat gia tri vao o do
     return 1;
 }
 
 void solve_sudoku(int t[N][N], int row, int col) {
+    //Khi da dien het cac hang va cot thi tang bien dem
     if (row == N - 1 && col == N) {
         count++;
         return;
     }
 
+    //Neu da dien het cac cot thi chuyen sang hang ke tiep
     if (col == N) {
-
         row++;
         col = 0;
     }
 
+    //Neu da ton tai gia tri thi chuyen den cot tiep theo
     if (t[row][col] != 0) {
-
         solve_sudoku(t, row, col + 1);
         return;
     }
 
-
+    //Thu lan luot cac gia tri tu 1-9 neu nhu chua ton tai gia tri
     for (int num = 1; num <= N; num++) {
         if (is_valid(t, row, col, num)) {
             t[row][col] = num;
             solve_sudoku(t, row, col + 1);
-            t[row][col] = 0; 
+            t[row][col] = 0; //backtracking de quay lai o truoc do 
         }
     }
 }
